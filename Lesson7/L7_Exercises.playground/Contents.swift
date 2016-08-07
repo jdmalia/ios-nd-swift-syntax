9c3ab35a7f4117ca6460e15dcde8f0e5b5a336db
Jason Malia <jmalia3@gatech.edu>
2016-08-06 21:53:28 -0400

    Complete lesson 7 exercises

--- Lesson7/L7_Exercises.playground/Contents.swift
+++ Lesson7/L7_Exercises.playground/Contents.swift
@@ -27,6 +27,9 @@ var espressoForGabrielle = macchiato
 espressoForGabrielle.steamedMilk = .Splash
 macchiato.steamedMilk
 
+// Struct .None
+// Class .Splash
+
 //: __Problem 2__
 //:
 //: __2a.__
@@ -35,75 +38,80 @@ macchiato.steamedMilk
 //: __2b.__
 //: Associate an Int value with each finger.
 
+enum Finger : Int {
+    case Thumb, Index, Middle, Ring, Pinky
+}
+Finger.Index.rawValue
+
 //: __Problem 3__
 //:
 //: Enum, class, or struct?
 //:
 //: Uncomment the code below and choose whether each type should be an enum, class, or struct.
-//____ Window {
-//    let height: Double
-//    let width: Double
-//    var open: Bool
-//}
+struct Window {
+    let height: Double
+    let width: Double
+    var open: Bool
+}
 
-//____ WritingImplement {
-//    case Pen
-//    case Pencil
-//    case Marker
-//    case Crayon
-//    case Chalk
-//}
+enum WritingImplement {
+    case Pen
+    case Pencil
+    case Marker
+    case Crayon
+    case Chalk
+}
 
-//____ Material {
-//    let name: String
-//    let density: Double
-//    let stiffness: Double
-//}
+struct Material {
+    let name: String
+    let density: Double
+    let stiffness: Double
+}
 
 
-//____ Bicycle {
-//    let frame: Material
-//    let weight: Double
-//    let category: String
-//
-//    static var bikeCategories: [String] = ["Road", "Touring", "Mountain", "Commuter", "BMX"]
-//
-//    func lookCool() {
-//        print("Check out my gear-shifters!")
-//    }
-//}
+struct Bicycle {
+    let frame: Material
+    let weight: Double
+    let category: String
 
-//____ Cyclist {
-//    var speed: Double
-//    let agility: Double
-//    let bike: Bicycle
-//
-//    var maneuverability: Double {
-//        get {
-//            return agility - speed/5)
-//        }
-//    }
-//
-//    init(speed: Double, agility: Double, bike: Bicycle) {
-//        self.speed = speed
-//        self.agility = agility
-//        self.bike = bike
-//    }
-//
-//    func brake() {
-//        speed--
-//    }
-//
-//    func pedalFaster(factor: Double) {
-//        speed * factor
-//    }
-//}
+    static var bikeCategories: [String] = ["Road", "Touring", "Mountain", "Commuter", "BMX"]
 
-//____ Size: String {
-//    case Small = "8 ounces"
-//    case Medium = "12 ounces"
-//    case Large = "16 ounces"
-//}
+    func lookCool() {
+        print("Check out my gear-shifters!")
+    }
+}
+
+class Cyclist {
+    var speed: Double
+    let agility: Double
+    let bike: Bicycle
+
+    var maneuverability: Double {
+        get {
+            return agility - speed/5
+        }
+    }
+
+    init(speed: Double, agility: Double, bike: Bicycle) {
+        self.speed = speed
+        self.agility = agility
+        self.bike = bike
+    }
+
+    func brake() {
+        speed -= 1
+    }
+
+    func pedalFaster(factor: Double) {
+        speed * factor
+    }
+}
+
+enum Size: String {
+    case Small = "8 ounces"
+    case Medium = "12 ounces"
+    case Large = "16 ounces"
+}
 
 //: __Problem 4__
 //:
@@ -121,6 +129,31 @@ macchiato.steamedMilk
 //: __4d.__
 //: Create an instance of your Cookie struct and call its method.
 
+enum CookieType {
+    case ChocolateChip, PeanutButter, ChocolateFudge
+}
+
+struct Cookie {
+    
+    
+    let flavor: CookieType
+    let storeBought: Bool
+    
+    var delicous : Bool {
+        get {
+            if !storeBought {
+                switch flavor {
+                case .ChocolateChip, .PeanutButter:
+                    return true
+                default:
+                    return false
+                }
+            }
+            return false
+        }
+    }
+}
+
 //: __Problem 5__
 //:
 //: Write a class to represent a listing for a Bed and Breakfast.
@@ -136,3 +169,42 @@ macchiato.steamedMilk
 
 //: __5d.__
 //: Create an instance of your BnBListing class and call one of its methods.
+
+enum PriceLevel {
+    case Cheap, Moderate, Expensive
+}
+
+class BnB {
+    
+    var continentalBreakfast: Bool
+    let priceLevel: PriceLevel
+    let numBedrooms: Int
+    var numBedroomsBooked: Int = 0
+    var vacancy: Bool {
+        get {
+            return numBedroomsBooked < numBedrooms
+        }
+    }
+    
+    init (numBedrooms: Int, priceLevel: PriceLevel, continentalBreakfast: Bool) {
+        self.continentalBreakfast = continentalBreakfast
+        self.numBedrooms = numBedrooms
+        self.priceLevel = priceLevel
+    }
+    
+    func book() -> Bool {
+        if self.vacancy {
+            numBedroomsBooked += 1
+            return true
+        }
+        return false
+    }
+}
+
+var casaDeSuenos = BnB(numBedrooms: 5, priceLevel: .Expensive, continentalBreakfast: true)
+casaDeSuenos.book()
+casaDeSuenos.book()
+casaDeSuenos.book()
+casaDeSuenos.book()
+casaDeSuenos.book()
+casaDeSuenos.book()
